79e9c7cb95759bd0047203b23f7c2b5efd46ef2d
galegoer <eric.m.galego@gmail.com>
2020-06-01 18:54:28 -0400

    fixed file renaming bug

--- playlistDownloader.py
+++ playlistDownloader.py
@@ -1,15 +1,19 @@
 import requests 
-from pytube import YouTube 
 import os
+from pytube import YouTube
  
-SAVE_PATH = "" #CHANGE TO PATH WHERE YOU WANT TO SAVE THE FILES
+SAVE_PATH = 'C:\\Users\\username\\Music\\myplaylist\\' #CHANGE TO PATH WHERE YOU WANT TO SAVE THE SONGS
 
-playlistLink = "" #PUT YOUR PUBLIC PLAYLIST LINK HERE
-key="" #insert a Youtube Data API v3 key here
+PLAYLIST_LINK = "https://www.youtube.com/playlist?list=PLArQ6Xij15ikMnefk5BL5PwdTLfqmtMNz" #PUT YOUR PUBLIC PLAYLIST LINK HERE
+KEY="" #insert a Youtube Data API v3 key here
+
+
 downloadLink = "https://www.youtube.com/watch?v="
 
-PLAYLIST_ID = playlistLink[playlistLink.find("=")+1:]
-final="https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId="+PLAYLIST_ID+"&key="+key
+PLAYLIST_ID = PLAYLIST_LINK[PLAYLIST_LINK.find("=")+1:]
+final="https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId="+PLAYLIST_ID+"&key="+KEY
+counter = 0
+
 
 r = requests.get(final)
 json = r.json()
@@ -23,25 +27,29 @@ for result in range(0, totalRes, perPage):
         currId = items[songId]["contentDetails"]["videoId"]
         try:
             yt = YouTube(downloadLink+currId)
-            print(yt.title)
-        except: 
-            print("Connection Error") 
+        except Exception as e:
+            print(e)
+            print("Error with pytube for video :" + downloadLink+currId)
+            continue
         d_video = yt.streams.filter(only_audio=True).first() #Change only_audio=True to file_extenstion='mp4' if you want mp4 files only
-        print(d_video)
+        #print(d_video)
         try: 
-            #downloading the video 
-            dfile = d_video.download()
-            os.rename(dfile, SAVE_PATH+yt.title)
-            
-            #os.rename(dfile, SAVE_PATH+yt.title+'.mp3') #THIS is if you want strictly mp3 files, uncomment this and comment out above line
-            
-            print("DOWNLOADED: "+ yt.title)
-        except: 
+            #downloading the video
+            dfile = d_video.download(SAVE_PATH)
+            if d_video.title == 'YouTube':
+                unknown_name = 'unknown_title'+str(counter)+'.mp4'
+                os.rename(dfile, SAVE_PATH+unknown_name)
+                counter += 1
+                print("DOWNLOADED: " + unknown_name) 
+            else:
+                print("DOWNLOADED: " + yt.title)
+        except Exception as e:
+            print(e)
             print("Error Downloading")
         
     if nextToken == None:
         break
-    final = "https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId="+PLAYLIST_ID+"&pageToken="+nextToken+"&key="+key
+    final = "https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId="+PLAYLIST_ID+"&pageToken="+nextToken+"&key="+KEY
     r = requests.get(final)
     json = r.json()
     nextToken = json.get("nextPageToken")
